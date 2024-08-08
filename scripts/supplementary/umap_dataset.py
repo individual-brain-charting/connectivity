c8b26b7c262c537ec53c4906ede46544a0bcdca2
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2024-08-08 17:01:28 +0200

    some umap plot formatting

--- scripts/supplementary/umap_dataset.py
+++ scripts/supplementary/umap_dataset.py
@@ -9,48 +9,33 @@ from sklearn.preprocessing import RobustScaler
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-
 results = "/storage/store3/work/haggarwa/connectivity/results/"
-plots = "/storage/store3/work/haggarwa/connectivity/plots/umap_dataset"
-os.makedirs(plots, exist_ok=True)
 
 # variables first set of connectomes
-n_parcels1 = 200
-tasktype1 = "natural"
-trim_length1 = None
-
-# variables second set of connectomes
-n_parcels2 = 200
-tasktype2 = "domain"
-trim_length2 = None
+n_parcels = 200
+tasktype = "natural"
+trim_length = None
 
 # load both sets of connectomes
-connectomes1 = pd.read_pickle(
+connectomes = pd.read_pickle(
     os.path.join(
         results,
-        f"connectomes_nparcels-{n_parcels1}_tasktype-{tasktype1}_trim-{trim_length1}.pkl",
+        f"connectomes_nparcels-{n_parcels}_tasktype-{tasktype}_trim-{trim_length}.pkl",
     )
 )
-connectomes2 = pd.read_pickle(
-    os.path.join(
-        results,
-        f"connectomes_nparcels-{n_parcels2}_tasktype-{tasktype2}_trim-{trim_length2}.pkl",
-    )
-)
+
 # label with trim length
-connectomes1["trim_length"] = trim_length1
-connectomes2["trim_length"] = trim_length2
+connectomes["trim_length"] = trim_length
 
 # dataset to keep
-datasets = ["ibc", "HCP900"]
-connectomes1 = connectomes1[connectomes1["dataset"].isin(datasets)]
-connectomes2 = connectomes2[connectomes2["dataset"].isin(datasets)]
+datasets = ["HumanMonkeyGBU", "ibc"]
+connectomes = connectomes[connectomes["dataset"].isin(datasets)]
 
 # tasks to keep
 tasks = [
     # "RestingState",
     # "Raiders",
-    # "GoodBadUgly",
+    "GoodBadUgly",
     # "MonkeyKingdom",
     # "Mario",
     # "LePetitPrince",
@@ -58,29 +43,54 @@ tasks = [
     # "ArchiSpatial",
     # "ArchiSocial",
     # "ArchiEmotional",
-    "HcpEmotion",
-    "HcpGambling",
-    "HcpLanguage",
-    "HcpMotor",
-    "HcpRelational",
-    # "lppFR",
-    "EMOTION",
-    "GAMBLING",
-    "LANGUAGE",
-    "MOTOR",
-    "RELATIONAL",
+    # "HcpEmotion",
+    # "HcpGambling",
+    # "HcpLanguage",
+    # "HcpMotor",
+    # "HcpRelational",
 ]
-connectomes1 = connectomes1[connectomes1["tasks"].isin(tasks)]
-connectomes2 = connectomes2[connectomes2["tasks"].isin(tasks)]
-
+connectomes = connectomes[connectomes["tasks"].isin(tasks)]
 
 # add tasktype column
-connectomes1["tasktype"] = [tasktype1] * len(connectomes1)
-connectomes2["tasktype"] = [tasktype2] * len(connectomes2)
+connectomes["tasktype"] = [tasktype] * len(connectomes)
 
-# combine both sets of connectomes
-connectomes = pd.concat([connectomes1, connectomes2], ignore_index=True)
+# reset index
 connectomes.reset_index(inplace=True, drop=True)
+
+# we only have three runs in common across ibc and HumanMonkeyGBU
+# so we will only keep those runs
+if "ibc" in datasets and "HumanMonkeyGBU" in datasets:
+    if "GoodBadUgly" in tasks:
+        # only keep run_labels run-03, run-04, run-05 for dataset ibc
+        ibc_connectomes = connectomes.loc[
+            np.where(
+                (
+                    connectomes["run_labels"].isin(
+                        ["run-03", "run-04", "run-05"]
+                    )
+                )
+                & (connectomes["dataset"] == "ibc")
+            )
+        ]
+
+        # only keep run_labels run-01, run-02, run-03 for dataset
+        # HumanMonkeyGBU
+        HumanMonkeyGBU_connectomes = connectomes.loc[
+            np.where(
+                (
+                    connectomes["run_labels"].isin(
+                        ["run-01", "run-02", "run-03"]
+                    )
+                )
+                & (connectomes["dataset"] == "HumanMonkeyGBU")
+            )
+        ]
+        # rename run labels to match across datasets
+        HumanMonkeyGBU_connectomes["run_labels"] = HumanMonkeyGBU_connectomes[
+            "run_labels"
+        ].map({"run-01": "run-03", "run-02": "run-04", "run-03": "run-05"})
+        connectomes = pd.concat([ibc_connectomes, HumanMonkeyGBU_connectomes])
+        del ibc_connectomes, HumanMonkeyGBU_connectomes
 
 # cov estimators
 cov_estimators = [
@@ -90,6 +100,9 @@ cov_estimators = [
 ]
 # connectivity measures for each cov estimator
 measures = ["correlation", "partial correlation"]
+
+plots = f"/storage/store3/work/haggarwa/connectivity/plots/umap_dataset/{datasets[0]}_{datasets[1]}"
+os.makedirs(plots, exist_ok=True)
 
 # loop over each cov estimator and measure
 for cov in cov_estimators:
@@ -107,23 +120,6 @@ for cov in cov_estimators:
             connectome_data = np.array(
                 connectomes_dropped[f"{cov} {measure}"].tolist()
             )
-            # get tasktype names
-            tasktype = np.array(connectomes_dropped["tasktype"].tolist())
-            # get task names
-            task = np.array(connectomes_dropped["tasks"].tolist())
-            # subject names
-            subject = np.array(connectomes_dropped["subject_ids"].tolist())
-            # dataset names
-            dataset = np.array(connectomes_dropped["dataset"].tolist())
-        else:
-            # get tasktype names
-            tasktype = np.array(connectomes["tasktype"].tolist())
-            # get task names
-            task = np.array(connectomes["tasks"].tolist())
-            # subject names
-            subject = np.array(connectomes["subject_ids"].tolist())
-            # dataset names
-            dataset = np.array(connectomes["dataset"].tolist())
 
         print(connectome_data.shape)
 
@@ -135,12 +131,25 @@ for cov in cov_estimators:
         reducer = umap.UMAP()
         embedding = reducer.fit_transform(connectome_data)
 
+        if cov == "Graphical-Lasso":
+            connectomes_dropped["umap1"] = embedding[:, 0]
+            connectomes_dropped["umap2"] = embedding[:, 1]
+        else:
+            connectomes["umap1"] = embedding[:, 0]
+            connectomes["umap2"] = embedding[:, 1]
+
         # # plot the UMAP representation
         sns.scatterplot(
-            x=embedding[:, 0],
-            y=embedding[:, 1],
-            hue=dataset,
-            style=task,
+            data=(
+                connectomes_dropped
+                if cov == "Graphical-Lasso"
+                else connectomes
+            ),
+            x="umap1",
+            y="umap2",
+            hue="dataset",
+            hue_order=datasets,
+            style="run_labels" if tasktype == "natural" else "tasks",
             palette="viridis",
             legend="full",
         )
@@ -154,7 +163,7 @@ for cov in cov_estimators:
         plt.savefig(
             os.path.join(
                 plots,
-                f"umap_comparetasktype_{cov}_{measure}_{datasets[0]}_{datasets[1]}_{trim_length1}_{trim_length2}.png",
+                f"umap_comparetasktype_{cov}_{measure}_{datasets[0]}_{datasets[1]}_{trim_length}.png",
             )
         )
         plt.close()
