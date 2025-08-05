22db0d94b211a2b0f3ea580230b6affda8e30891
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-08-05 14:20:23 +0200

    try another partition

--- scripts/slurm/slurm_0-1_across_fc_classification.sh
+++ scripts/slurm/slurm_0-1_across_fc_classification.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=0.1_across
-#SBATCH -c10
+#SBATCH -c20
 #SBATCH --output=logs/across_0.1_jobid_%A_%a.out
 #SBATCH --error=logs/across_0.1_jobid_%A_%a.err
-#SBATCH --partition=normal,parietal
+#SBATCH --partition=parietal
 
 srun python /data/parietal/store3/work/haggarwa/connectivity/scripts/supplementary/classify_fc_var_lowpass.py 0.1 across
