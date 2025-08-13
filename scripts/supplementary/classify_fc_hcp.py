41f291009453b1eca66f0cc6489a529de29c9474
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-08-13 13:25:34 +0200

    fix

--- scripts/supplementary/classify_fc_hcp.py
+++ scripts/supplementary/classify_fc_hcp.py
@@ -52,7 +52,7 @@ connectivity_measures = [
 # cache and root output directory
 data_root = "/data/parietal/store3/work/haggarwa/connectivity/data/"
 # results directory
-results = "/data/parietal/store3/work/haggarwa/connectivity/results/wo_extra_GBU_runs"
+results = "/data/parietal/store3/work/haggarwa/connectivity/results/"
 os.makedirs(results, exist_ok=True)
 
 if within_task:
