45cb6d13a65b39b67d07e5243a3dfc214e90f068
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-08-13 13:22:08 +0200

    fix

--- scripts/slurm/slurm_hcp.sh
+++ scripts/slurm/slurm_hcp.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=hcp_classify_fc
 #SBATCH -c40
 #SBATCH --partition=parietal,normal
-#SBATCH --error jobid_%A.err
-#SBATCH --output=logs/jobid_%A.out
+#SBATCH --error=jobid_%A.err
+#SBATCH --output=jobid_%A.out
 
 srun python /data/parietal/store3/work/haggarwa/connectivity/scripts/supplementary/classify_fc_hcp.py
