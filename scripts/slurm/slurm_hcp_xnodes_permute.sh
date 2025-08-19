f4863a73d55e3366506e0db5f93a54a0ce8c11ed
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-08-19 14:32:53 +0200

    slurm script for permutation

---
+++ scripts/slurm/slurm_hcp_xnodes_permute.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=hcp_classify_fc
+#SBATCH -c63
+#SBATCH --partition=normal,parietal
+#SBATCH --error=logs/jobid_%A_%a.err
+#SBATCH --output=logs/jobid_%A_%a.out
+#SBATCH --array=1-63
+
+# Get the specific line from tmp_combinations.txt based on the array task ID
+COMBINATIONS_FILE="/data/parietal/store3/work/haggarwa/connectivity/data/hcp_combinations.txt"
+LINE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" $COMBINATIONS_FILE)
+
+# Parse the line to extract task1, task2, and connectivity_measure
+read -r TASK1 TASK2 CONNECTIVITY_MEASURE <<< "$LINE"
+
+# Run the Python script with the extracted arguments
+srun python /data/parietal/store3/work/haggarwa/connectivity/scripts/supplementary/classify_permute_subjects_hcp_slurm_compatible.py "$TASK1" "$TASK2" "$CONNECTIVITY_MEASURE"
