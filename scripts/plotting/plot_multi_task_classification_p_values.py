3c47557075585a614295aa3a2bfc14c9ac2a664d
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-08-19 10:23:39 +0200

    nadeau bengio test

--- scripts/plotting/plot_multi_task_classification_p_values.py
+++ scripts/plotting/plot_multi_task_classification_p_values.py
@@ -2,26 +2,25 @@ import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sys
 
 # add utils to path
-# sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from utils.plot import wrap_labels
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from utils.plot import wrap_labels, compute_corrected_ttest
 from scipy.stats import wilcoxon, ttest_rel
 
-results_root = "/data/parietal/store3/work/haggarwa/connectivity/results/wo_extra_GBU_runs"
-plots_root = (
-    "/data/parietal/store3/work/haggarwa/connectivity/plots/wo_extra_GBU_runs"
-)
+root = "/Users/himanshu/Desktop/ibc/connectivity"
+# root = "/data/parietal/store3/work/haggarwa/connectivity"
+results_root = "results/wo_extra_GBU_runs"
+
 n_parcels = 400
 trim_length = None
 tasktype = "natural"
 do_hatch = False
 
 dir_name = f"classification-across_tasktype-{tasktype}_nparcels-{n_parcels}_trim-{trim_length}"
-results_pkl = os.path.join(results_root, dir_name, "all_results.pkl")
+results_pkl = os.path.join(root, results_root, dir_name, "all_results.pkl")
 
-output_dir = os.path.join(plots_root, dir_name)
-os.makedirs(output_dir, exist_ok=True)
 df = pd.read_pickle(results_pkl)
 
 # cov estimators
@@ -40,7 +39,6 @@ for score in ["balanced_accuracy", "f1_macro"]:
         p_values[score][clas] = {}
         df_ = df[df["classes"] == clas]
         df_.reset_index(inplace=True, drop=True)
-
         for between in [
             "Unregularized correlation vs. Graphical-Lasso partial correlation",
             "Ledoit-Wolf correlation vs. Graphical-Lasso partial correlation",
@@ -62,3 +60,27 @@ for score in ["balanced_accuracy", "f1_macro"]:
             p_values[score][clas][between]["ttest_rel"] = ttest_rel(
                 scores_1, scores_2, alternative=alt
             )[1]
+
+            # scores of the best model
+            model_1_scores = scores_1.values
+            # scores of the second-best model
+            model_2_scores = scores_2.values
+            if alt == "greater":
+                differences = model_1_scores - model_2_scores
+            else:
+                differences = model_2_scores - model_1_scores
+
+            n = differences.shape[0]  # number of test sets
+            dof = n - 1
+            n_train = df_["train_sets"].loc[0].shape[0]
+            n_test = df_["test_sets"].loc[0].shape[0]
+
+            p_values[score][clas][between]["corrected_ttest"] = (
+                compute_corrected_ttest(differences, dof, n_train, n_test)[1]
+            )
+            print(
+                f"{between} {clas} {score}: "
+                f"wilcoxon: {p_values[score][clas][between]['wilcoxon']}, "
+                f"ttest_rel: {p_values[score][clas][between]['ttest_rel']}, "
+                f"corrected_ttest: {p_values[score][clas][between]['corrected_ttest']}"
+            )
--- utils/plot.py
+++ utils/plot.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.svm import LinearSVC
 import textwrap
+from scipy.stats import t
 
 
 def wrap_labels(ax, width, break_long_words=False):
@@ -457,7 +458,13 @@ def mean_connectivity(data, tasks, cov_estimators, measures):
                 for measure in measures:
                     connectivity = df[cov + " " + measure].tolist()
                     connectivity = np.array(connectivity)
-                    connectivity = connectivity.mean(axis=0)
+                    connectivity = np.nanmean(connectivity, axis=0)
+                    # skip if connectivity is all nan
+                    if np.isnan(connectivity).all():
+                        print(
+                            f"Skipping {task} {sub} {cov} {measure} due to all NaN connectivity"
+                        )
+                        continue
                     av_connectivity.append(
                         {
                             "task": task,
@@ -507,3 +514,56 @@ def insert_stats_reliability(
     )
     ax.set_xticks([*range(0, x_n)])
     ax.axis("off")
+
+
+def corrected_std(differences, n_train, n_test):
+    """Corrects standard deviation using Nadeau and Bengio's approach.
+
+    Parameters
+    ----------
+    differences : ndarray of shape (n_samples,)
+        Vector containing the differences in the score metrics of two models.
+    n_train : int
+        Number of samples in the training set.
+    n_test : int
+        Number of samples in the testing set.
+
+    Returns
+    -------
+    corrected_std : float
+        Variance-corrected standard deviation of the set of differences.
+    """
+    # kr = k times r, r times repeated k-fold crossvalidation,
+    # kr equals the number of times the model was evaluated
+    kr = len(differences)
+    corrected_var = np.var(differences, ddof=1) * (1 / kr + n_test / n_train)
+    corrected_std = np.sqrt(corrected_var)
+    return corrected_std
+
+
+def compute_corrected_ttest(differences, df, n_train, n_test):
+    """Computes right-tailed paired t-test with corrected variance.
+
+    Parameters
+    ----------
+    differences : array-like of shape (n_samples,)
+        Vector containing the differences in the score metrics of two models.
+    df : int
+        Degrees of freedom.
+    n_train : int
+        Number of samples in the training set.
+    n_test : int
+        Number of samples in the testing set.
+
+    Returns
+    -------
+    t_stat : float
+        Variance-corrected t-statistic.
+    p_val : float
+        Variance-corrected p-value.
+    """
+    mean = np.mean(differences)
+    std = corrected_std(differences, n_train, n_test)
+    t_stat = mean / std
+    p_val = t.sf(np.abs(t_stat), df)  # right-tailed t-test
+    return t_stat, p_val
