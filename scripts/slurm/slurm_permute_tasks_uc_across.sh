1e81b2d892d70b6383d7d41df618be2933a472f8
Himanshu Aggarwal <himanshuaggarwal1997@gmail.com>
2025-08-22 16:28:13 +0200

    more permute scripts

---
+++ scripts/slurm/slurm_permute_tasks_uc_across.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH --job-name=permute
+#SBATCH -c50
+#SBATCH --partition=parietal,normal
+#SBATCH --error=logs/jobid_%A.err
+#SBATCH --output=logs/jobid_%A.out
+
+# Run the Python script with the extracted arguments
+srun python /data/parietal/store3/work/haggarwa/connectivity/scripts/supplementary/permutation_testing.py 0.2 across Tasks "Unregularized correlation"
